c14e23ec32d315c808c55c4add35078e805e3bf6
sadpvndv <andreseloy08@gmail.com>
2019-11-26 00:13:41 -0500

    Add Coming soon

---
+++ components/ComingSoon.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ComingSoon = () => (
+    <div className="main">
+    <span>
+        <img src="cs.png" className="main__img" alt="Icono 1"></img>
+    </span>
+    <style jsx>{`
+      .main__img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 700px;
+      }
+    `}</style>
+    </div>
+);
+
+export default ComingSoon;
